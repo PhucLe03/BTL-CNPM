307060374a7f860056a910badb3c6d1366bee40e
PhucLe03 <hailua112003@gmail.com>
2023-12-10 17:22:02 +0700

    Update readme

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
+const data = require('./credentials.json');
+
 const db = mysql.createConnection(
     {
         host: "localhost",
-        user: "root",
-        password: "L@c02032003",
+        user: data.user || "root",
+        password: data.password || "",
         database: "QUANLYINAN",
     },
     function (err) {
